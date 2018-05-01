88813899bf0eea65687d90a32b898d7efe1fd842
dirchev <dimitar.mirchev96@gmail.com>
2018-05-01 22:51:23 +0100

    Bootstrao the 3 pages in the app

---
+++ src/components/CakePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class CakePage extends Component {
+  render() {
+    return (
+      <div>
+        <div className="hero is-primary">
+          <div className="hero-body">
+            <div className="title">CakePage</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CakePage;
