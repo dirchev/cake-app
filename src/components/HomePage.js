6faa8217755b0a8b940764c943eb06629e6c65b2
dirchev <dimitar.mirchev96@gmail.com>
2018-05-05 01:11:19 +0100

    Update HomePage
    * shows a list of cakes
    * for each cake uses CakePreview

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import CakePreview from './CakePreview'
+import Navigation from './Navigation'
+import PropTypes from 'prop-types'
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        <div className="hero is-primary">
-          <div className="hero-body">
-            <div className="title">Cake App</div>
-            <div className="subtitle">Waracle Challenge</div>
-          </div>
+      <div className="card">
+        <Navigation />
+        <div className="card-content">
+          <div className="columns">
+              {
+                this.props.cakes.map((cake) => (
+                  <div key={cake.id} className="column is-one-third-desktop is-half-tablet">
+                    <CakePreview {...cake} />
+                  </div>
+                ))
+              }
+            </div>
         </div>
       </div>
     )
   }
 }
 
-export default HomePage;
+HomePage.propTypes = {
+  cakes: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    imgURL: PropTypes.string.isRequired,
+    comment: PropTypes.string.isRequired,
+    yumFactor: PropTypes.number.isRequired
+  })).isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cakes: Object.keys(state.cakes).map((cakeId) => state.cakes[cakeId])
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
