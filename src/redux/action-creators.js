0dfad32f1c8b6c27757663a2ab1ebc74d818ac14
dirchev <dimitar.mirchev96@gmail.com>
2018-05-05 03:01:38 +0100

    Update action-creators - add actions
    1. Wire create cake with API
    2. Add loadAllCakes
    3. Add discardCakeCreation

--- src/redux/action-creators.js
+++ src/redux/action-creators.js
@@ -1,22 +1,69 @@
 import { push } from 'react-router-redux'
 import uuid from 'uuid'
+import CakeApi from '../utils/api'
+
+const api = new CakeApi()
 
 export default {
   createCake: (data) => (dispatch) => {
-    data.id = uuid.v4()
+    // used to keep track of the cake until it receives an ID from the API
+    var creationId = uuid.v4()
 
     dispatch({
       type: 'START_CREATE_CAKE',
-      payload: data,
+      payload: {...data, id: creationId}
     })
-
     dispatch(push('/'))
 
-    setTimeout(() => {
-      dispatch({
-        type: 'SUCCESS_CREATE_CAKE',
-        payload: data,
+    api.create(data)
+      .then(({data}) => {
+        dispatch({
+          type: 'SUCCESS_CREATE_CAKE',
+          payload: {...data, creationId}
+        })
       })
-    }, 1000)
+      .catch(({response}) => {
+        let err = response.data
+        dispatch({
+          type: 'ERROR_CREATE_CAKE',
+          payload: {...data, id: creationId},
+          error: err
+        })
+      })
+  },
+  discardCakeCreation: (cakeId) => (dispatch) => {
+    dispatch({
+      type: 'DISCARD_CAKE_CREATION',
+      payload: {id: cakeId}
+    })
+  },
+  loadAllCakes: () => (dispatch) => {
+    dispatch({
+      type: 'TOGGLE_GLOBAL_LOADING',
+      payload: {loading: true}
+    })
+
+    api.getAll()
+      .then(({data}) => {
+        dispatch({
+          type: 'TOGGLE_GLOBAL_LOADING',
+          payload: {loading: false}
+        })
+        dispatch({
+          type: 'SYNC_CAKES',
+          payload: data
+        })
+      })
+      .catch(({response}) => {
+        const errors = response.data
+        dispatch({
+          type: 'TOGGLE_GLOBAL_LOADING',
+          payload: {loading: false}
+        })
+        dispatch({
+          type: 'GLOBAL_LOADING_FAIL',
+          payload: {errors: errors}
+        })
+      })
   }
 }
